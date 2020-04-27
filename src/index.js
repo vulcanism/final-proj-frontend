6f6fe8786658181d40f6ee45d3510df756c70feb
Savannah C <cynomania@gmail.com>
2020-04-27 11:24:01 -0400

    imported packages

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import App from './App';
+import { Provider } from 'react-redux'
 
 ReactDOM.render(
   <React.StrictMode>
