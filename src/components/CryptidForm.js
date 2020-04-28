b4fad0269e2c6ffb4593c2ae266f9d394d7412f5
Savannah C <cynomania@gmail.com>
2020-04-28 12:59:55 -0400

    setting up form, may refactor later

--- src/components/CryptidForm.js
+++ src/components/CryptidForm.js
@@ -1,9 +1,24 @@
 import React from "react";
 
-const CryptidForm = (props) => {
+class CryptidForm extends React.Component {
+    render() {
+
     return (
-        <div>
-            CryptidForm
+        <div className="cryptid-form">
+            <form>
+                <label>Name:</label>
+                <input type="text" placeholder="Name"/><br/>
+
+                <label>Size:</label>
+                <input type="text" placeholder="Size"/><br/>
+
+                <label>Classification:</label>
+                <input type="text" placeholder="Classification"/><br/>
+            </form>
+            
         </div>
     )
+    }
 }
+
+export default CryptidForm
