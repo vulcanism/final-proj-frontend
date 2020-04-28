0fcb04f6972c2b6e5ecedf9b8c163aee1b8e82d2
Savannah C <cynomania@gmail.com>
2020-04-28 13:44:16 -0400

    form and action to add new cryptid

--- src/components/CryptidForm.js
+++ src/components/CryptidForm.js
@@ -1,23 +1,41 @@
 import React from "react";
 
 class CryptidForm extends React.Component {
+
+    state = {
+        name: "",
+        size: "",
+        classification: ""
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+    }
+
     render() {
+        return (
+            <div className="cryptid-form">
+                <form onSubmit={this.handleSubmit}>
+                    <label>Name:</label>
+                    <input type="text" placeholder="Name" value={this.state.name} name="name" onChange={this.handleChange}/><br/>
 
-    return (
-        <div className="cryptid-form">
-            <form>
-                <label>Name:</label>
-                <input type="text" placeholder="Name"/><br/>
+                    <label>Size:</label>
+                    <input type="text" placeholder="Size" value={this.state.size} name="size" onChange={this.handleChange}/><br/>
 
-                <label>Size:</label>
-                <input type="text" placeholder="Size"/><br/>
+                    <label>Classification:</label>
+                    <input type="text" placeholder="Classification" value={this.state.classification} name="classification" onChange={this.handleChange}/><br/>
 
-                <label>Classification:</label>
-                <input type="text" placeholder="Classification"/><br/>
-            </form>
-            
-        </div>
-    )
+                    <input type="submit"/>
+                </form>
+                
+            </div>
+        )
     }
 }
 
