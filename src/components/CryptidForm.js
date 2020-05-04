333178d92219e82620885f4ffaf85b80b56ac350
Savannah C <cynomania@gmail.com>
2020-05-04 11:15:41 -0400

    spacing

--- src/components/CryptidForm.js
+++ src/components/CryptidForm.js
@@ -30,13 +30,13 @@ class CryptidForm extends React.Component {
         return (
             <div className="cryptid-form">
                 <form onSubmit={this.handleSubmit}>
-                    <label>Name:</label>
+                    <label>Name: </label>
                     <input type="text" placeholder="Name" value={this.state.name} name="name" onChange={this.handleChange}/><br/>
 
-                    <label>Size:</label>
+                    <label>Size: </label>
                     <input type="text" placeholder="Size" value={this.state.size} name="size" onChange={this.handleChange}/><br/>
 
-                    <label>Classification:</label>
+                    <label>Classification: </label>
                     <input type="text" placeholder="Classification" value={this.state.classification} name="classification" onChange={this.handleChange}/><br/>
 
                     <input type="submit"/>
