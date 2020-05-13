82e8c13c82d5e8adc93b9ab66561b40aed280d94
Savannah C <cynomania@gmail.com>
2020-05-13 16:52:42 -0400

    beginning to set up new sighting form

--- src/components/Sightings/SightingForm.js
+++ src/components/Sightings/SightingForm.js
@@ -1,14 +1,19 @@
 import React from "react";
+import {connect} from "react-redux"
 
 class SightingForm extends React.Component {
 
     render() {
         return (
             <div>
-                Sighting Form
+                <form>
+                    <label>Date:</label>
+                    <label>Location:</label>
+                    <label>Note:</label>
+                </form>
             </div>
         )
     }
 }
 
-export default SightingForm;
+export default connect()(SightingForm);
