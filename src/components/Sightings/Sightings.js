b1da565d2a0bba0487623555b9fdf927a43e9d8a
Savannah C <cynomania@gmail.com>
2020-05-13 16:42:50 -0400

    cryptid page listing sightings

--- src/components/Sightings/Sightings.js
+++ src/components/Sightings/Sightings.js
@@ -3,7 +3,9 @@ import React from "react";
 const Sightings = (props) => {
     return (
         <div>
-            Sightings
+            {props.sightings && props.sightings.map(sighting => 
+                <li key={sighting.id}>{sighting.date}</li>            
+            )}
         </div>
     )
 }
