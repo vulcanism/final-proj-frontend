28c4f455c6d0666c212c5bcc81407cfc06a7fbdd
Savannah C <cynomania@gmail.com>
2020-05-14 15:27:37 -0400

    delete sighting action

--- src/actions/deleteSighting.js
+++ src/actions/deleteSighting.js
@@ -1,5 +1,9 @@
-export const deleteSighting = (sightingId) => {
+export const deleteSighting = (sightingId, cryptidId) => {
     return (dispatch) => {
-
+        return fetch(`http://localhost:3000/cryptids/${cryptidId}/sightings/${sightingId}`, {
+            method: "DELETE"
+        })
+        .then(response => response.json())
+        .then(cryptid => dispatch({type: "DELETE_SIGHTING", payload: cryptid}))
     }
 }
