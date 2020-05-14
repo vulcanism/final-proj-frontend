f33f12ec9ff1ba5c53dd0da2c434c4fa929f1e67
Savannah C <cynomania@gmail.com>
2020-05-14 15:23:25 -0400

    setting up delete sighting function

--- src/components/Sightings/Sightings.js
+++ src/components/Sightings/Sightings.js
@@ -1,10 +1,18 @@
 import React from "react";
 
 const Sightings = (props) => {
+
+    const handleDelete = (sighting) => {
+        
+    }
+
     return (
         <div>
             {props.sightings && props.sightings.map(sighting => 
-                <li key={sighting.id}>{sighting.date}</li>            
+                <li key={sighting.id}>
+                    {sighting.date} - {sighting.location}
+                    <button onClick={handleDelete}>Delete</button>                    
+                </li>            
             )}
         </div>
     )
