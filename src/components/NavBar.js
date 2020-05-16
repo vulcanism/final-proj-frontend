10e6955cc8c67ff9362f3d87aae0a091557cfec9
Savannah C <cynomania@gmail.com>
2020-05-16 15:08:56 -0400

    navigation links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,8 @@ import {Link} from "react-router-dom";
 const NavBar = (props) => {
     return (
         <div>
-            Navigation
+            <Link to="/">Home /</Link>
+            <Link to="/cryptids"> Cryptids</Link>
         </div>
     )
 }
--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import {Route} from "react-router-dom";
 import Home from "../components/Home";
+import NavBar from "../components/NavBar";
 
 import CryptidForm from "../components/Cryptids/CryptidForm";
 import CryptidsContainer from "./CryptidsContainer"
@@ -11,6 +12,7 @@ class Main extends Component {
     render() {
         return (
             <div>
+                <NavBar />
                 <Route exact path="/" render={Home}/>
                 <Route exact path="/cryptids/new" component={CryptidForm} />
                 <Route exact path="/cryptids" component={CryptidsContainer} />
